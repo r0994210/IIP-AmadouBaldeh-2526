d31bb0822721636d1b7fadf3ef34b5cc814d4b86
r0994210 <amadou.baldeh@student.odisee.be>
2025-10-01 15:21:47 +0200

    juiste push

--- IIP1.01.Basis/ConsoleGangsterName/Program.cs
+++ IIP1.01.Basis/ConsoleGangsterName/Program.cs
@@ -7,17 +7,24 @@ internal class Program
         Console.WriteLine(@"***********************
 | GANSTA NAME BUILDER |
 ***********************");
-
+		Console.WriteLine();
+		Console.WriteLine();
+		
         Console.Write("Give the first name of any Disney character: ");
         string DisneyNaam = Console.ReadLine();
+		
         Console.Write("Give any workbench tool: ");
         string WorkbenchTool = Console.ReadLine();
+		
         Console.Write("What is your last name: ");
         string LastName = Console.ReadLine();
+		
         Console.WriteLine();
+		
         Console.Write("Your gansta name: ");
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine( DisneyNaam + $" 'the {WorkbenchTool} '" + LastName);
+		
         Console.ReadKey();
     }
 }
