bdc05e997ddd859232bee5670bf8eeb79e3bd194
r0994210 <amadou.baldeh@student.odisee.be>
2025-10-10 00:03:10 +0200

    pushhh

---
+++ IIP1.02.Variabelen/ConsoleContactCard/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+		Console.WriteLine("");
+		string Naam = "amadou";
+		string gehuwd = "nee";
+		double nummer = 04.86332219;
+		int leeftijd = 25;
+		double salaris = 2.50000;
+		string geslacht = "m";
+		double lengte = 1.75;
+		Console.WriteLine($"Naam: {Naam}");
+		Console.WriteLine($"Gehuwd:{gehuwd}");
+		Console.WriteLine($"telefoon: {nummer}");
+		Console.WriteLine($"leeftijd:{leeftijd} ");
+		Console.WriteLine($"salaris:{salaris} ");
+		Console.WriteLine($"geslacht:{geslacht} ");
+		Console.WriteLine($"lengte:{lengte} ");
+		Console.WriteLine("");
+
+    }
+}
